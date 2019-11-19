5085fc59707d2604d7d67c8c1ff24783f4e47f85
xochy <xochy@DESKTOP-XOCHY>
2019-11-19 01:09:21 -0600

    Se actualizan los archivos en netbeans

---
+++ prueba/src/main/java/cap02/TestFechaHora.java
@@ -0,0 +1,10 @@
+package cap02;
+
+public class TestFechaHora{
+
+    public static void main(String[] args) {
+        
+        FechaHora fh = new FechaHora("12/11/1990", 4, 35, 69);
+        System.out.println(fh); 
+    }
+}
