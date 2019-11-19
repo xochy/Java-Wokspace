563b2eaefecbba438f9678da4c09e2098611ffd8
xochy <xochydeather@gmail.com>
2019-11-18 22:33:34 -0600

    ultimo commit java

---
+++ libro/src/cap02/colecciones/MiColeccion.java
@@ -0,0 +1,40 @@
+package cap02.colecciones;
+
+public class MiColeccion{
+
+    private Object datos[] = null;
+    private int len        = 0;
+
+    public MiColeccion(int capacidadInicial){
+
+        datos = new Object[capacidadInicial];
+    }
+
+    public Object obtener(int pos){
+        
+        return datos[pos];
+    }
+
+    public int cantidad(){
+
+        return len;
+    }
+
+    public void insertar(Object elm, int i){
+
+        if(len == datos.length){
+            
+            Object aux[] = datos;
+            datos        = new Object[datos.length * 2];
+
+            for(int j = 0; j < len; j++){
+
+                datos[j] = aux[j];
+            }
+        }
+
+        for(int j = len - 1; j >= i; j--){
+
+        }
+    }
+}
