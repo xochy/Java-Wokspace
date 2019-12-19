1893f096d8470e3c9dac05d9a152ffeac5451ca6
xochy <xochydeather@gmail.com>
2019-12-18 19:20:36 -0600

    Se suben cambios de colecciones

--- libro/src/cap02/colecciones/MiColeccion.java
+++ libro/src/cap02/colecciones/MiColeccion.java
@@ -1,39 +1,40 @@
 package cap02.colecciones;
 
-public class MiColeccion{
+public class MiColeccion {
 
     private Object datos[] = null;
-    private int len        = 0;
+    private int len = 0;
 
-    public MiColeccion(int capacidadInicial){
+    public MiColeccion(int capacidadInicial) {
 
         datos = new Object[capacidadInicial];
     }
 
-    public Object obtener(int pos){
-        
+    // *Este método debe ser rediseñado
+    public Object obtener(int pos) {
+
         return datos[pos];
     }
 
-    public int cantidad(){
+    public int cantidad() {
 
         return len;
     }
 
-    public void insertar(Object elm, int i){
+    public void insertar(Object elm, int i) {
 
-        if(len == datos.length){
-            
+        if (len == datos.length) {
+
             Object aux[] = datos;
-            datos        = new Object[datos.length * 2];
+            datos = new Object[datos.length * 2];
 
-            for(int j = 0; j < len; j++){
+            for (int j = 0; j < len; j++) {
 
                 datos[j] = aux[j];
             }
         }
 
-        for(int j = len - 1; j >= i; j--){
+        for (int j = len - 1; j >= i; j--) {
 
         }
     }
